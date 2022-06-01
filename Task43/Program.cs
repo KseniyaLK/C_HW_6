0b68c0071bb094e64ecdf55a80a98405e72f0d1a
Kseniya Kudryavtseva <kseniya756@bk.ru>
2022-06-01 20:29:56 +0300

    Task43_1

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -10,14 +10,21 @@ double b1 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите значение k1: ");
 double k1 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите значение b2: ");
-double b2 = Convert.ToDouble(Console.ReadLine());
+double b2 = Convert.ToDouble(Console.ReadLine()); 
 Console.WriteLine("Введите значение k2: ");
 double k2 = Convert.ToDouble(Console.ReadLine());
 
+double x = 0;
+double y = 0;
+double IntersectionLines(double b1, double k1, double b2, double k2)
+{
 
+    x = (b2 - b1) / (k1 - k2);
+    y = k1 * x + b1;
+    return (x);
+    return (y);
 
-
-double x = (b2 - b1) / (k1 - k2);
-double y = k1 * x + b1;
-
+}
+double result = IntersectionLines(b1, k1, b2, k2);
 Console.WriteLine($"Точка пересечения двух прямых ({x}; {y})");
+
